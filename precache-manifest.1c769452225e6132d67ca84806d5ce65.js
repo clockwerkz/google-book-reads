dead68e75a38ac0a3d9cf9588bda11667a0e561f
clockwerkz <clockwerkz@gmail.com>
2019-03-01 14:09:50 -0800

    Updates

---
+++ precache-manifest.1c769452225e6132d67ca84806d5ce65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0087a7feacac7bd5af92",
+    "url": "/google-book-reads/static/css/main.26703907.chunk.css"
+  },
+  {
+    "revision": "0087a7feacac7bd5af92",
+    "url": "/google-book-reads/static/js/main.0087a7fe.chunk.js"
+  },
+  {
+    "revision": "a67f89cea1d055e16caa",
+    "url": "/google-book-reads/static/js/runtime~main.a67f89ce.js"
+  },
+  {
+    "revision": "b14ba49ae75063c5ef69",
+    "url": "/google-book-reads/static/js/2.b14ba49a.chunk.js"
+  },
+  {
+    "revision": "e18133b182cf740a3d75fb5acd254b26",
+    "url": "/google-book-reads/index.html"
+  }
+];
